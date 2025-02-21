efb33e1e90a35d3f6bf0011ab8d55455576631b6
I-Am-Shimanto <shimantowebdeveloper@gmail.com>
2025-02-21 23:06:28 +0600

    new arrivals done

--- src/Components/newArrivals/NewArrivals.jsx
+++ src/Components/newArrivals/NewArrivals.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import './newarrivals.css'
 
 const NewArrivals = () => {
     const [userData , setUserData] = useState([])
@@ -9,18 +11,19 @@ const NewArrivals = () => {
             .then(response => response.json())
             .then(json => setUserData(json))
       } , [])
-console.log(userData);
 
   return (
     <>
-        <div className="container">
-            <div className="row flex gap-6 flex-wrap">
+        <div id='newarrivals' className="container">
+        <h2 className='text-2xl font-normal font-poppins text-black leading-9 text-center my-6'>New <span className='text-brand font-bold'>Arrival</span></h2>
+            <div className="row flex gap-6 flex-wrap justify-center overflow-hidden">
                 {
                     userData.map((item)=>(
-                        <div className="single_card w-[250px] border">
-                            <img src={item.image} alt="" />
-                            <p>{item.description}</p>
-                            <h3>{item.price}</h3>
+                        <div className="single_card w-[220px] h-[380px] cursor-pointer border-[1px] border-brand shadow-xl p-3 flex flex-col relative">
+                            <img className='h-fit' src={item.image} alt="image" />
+                            <h3 className='text-lg font-semibold font-poppins text-brand mb-2'>{item.price}</h3>
+                            <p className='text-xs font-poppins font-medium'>{item.description}</p>
+                            <Link className='cart w-[80%] py-2 bg-brand rounded-xl text-center text-white font-semibold font-poppins text-sm mt-5'>Add to Cart</Link>
                         </div>
                     ))
                 }
